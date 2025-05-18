1b55538cfe1dcf689acd0a48f225e775f669d1ee
khaledalsadi11~ <alsadikhaled2003@gmail.com>
2025-05-18 13:26:36 +0300

    Shipping date

--- WebApplication1 - Copy - Copy/WebApplication1/obj/Debug/net5.0/WebApplication1.RazorTargetAssemblyInfo.cs
+++ WebApplication1 - Copy - Copy/WebApplication1/obj/Debug/net5.0/WebApplication1.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acffe9e188c89a34502298fdb8a5c12edb188336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1116a982091b465c63b64a5f771226ef0ee688b1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
